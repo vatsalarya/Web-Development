a2e928d9505edf8f157663277b5713b89fb65e4a
Vatsal Arya <vatsalarya1@gmail.com>
2020-12-02 14:36:37 +0530

    added todolist

---
+++ HTML Complete Web Dev Bootcamp/todo_list_v1/app.js
@@ -0,0 +1,34 @@
+const express = require("express");
+const bodyParser = require("body-parser");
+var items = [];
+const app = express();
+
+app.set('view engine', 'ejs');
+
+app.use(bodyParser.urlencoded({extended: true}));
+app.use(express.static("public"));
+
+app.get("/", function(req, res) {
+    var today = new Date(); 
+
+    var options = {
+        weekday: "long",
+        day: "numeric",
+        month: "long"
+    };
+
+    var day = today.toLocaleDateString("en-us", options);
+
+    res.render("list", {kindOfDay: day, items: items});
+});
+
+app.post("/", function(req, res) {
+    var item = req.body.newItem;
+    items.push(item);
+
+    res.redirect("/");
+});
+
+app.listen(3000, function() {
+    console.log("Server Started at port 3000");
+});
