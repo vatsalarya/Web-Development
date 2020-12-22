6d52c496d9adfd5466093dca2ef41463e1de2a52
Vatsal Arya <vatsalarya1@gmail.com>
2020-12-22 05:52:17 +0530

    completed wiki api

--- HTML Complete Web Dev Bootcamp/Wiki-API/app.js
+++ HTML Complete Web Dev Bootcamp/Wiki-API/app.js
@@ -21,7 +21,9 @@ const articleSchema = {
 
 const Article = mongoose.model("Article", articleSchema);
 
-app.route()
+//_______________________Requests targating all articles________________________//
+
+app.route("/articles")
 
 .get(function(req, res){
   Article.find({}, function(err, foundArticles){
@@ -58,7 +60,50 @@ app.route()
   })
 })
 
-//TODO
+//_______________________Requests targating specifice articles______________________
+
+app.route("/articles/:articleTitle")
+
+.get(function(req, res){
+  Article.findOne({title: req.params.articleTitle}, function(err){
+    if(foundArticles){
+      res.send(foundArticles);
+    } else{
+      res.send("No Articles Matching");
+    }
+  });
+})
+
+.put(function(req, res){
+  Article.update({title: req.params.articleTitle}, {title: req.body.title, content: req.body.content}, {overwrite: true}, function(err){
+    if(!err){
+      res.send("Sucess");
+    } else{
+      res.send("err");
+    }
+  });
+})
+
+.patch(function(req, res){
+  Article.update({title: req.params.articleTitle}, {$set: req.body}, function(err){
+    if(!err){
+      res.send("Sucess");
+    } else{
+      res.send("err");
+    }
+  });
+})
+
+.delete(function(req, res){
+  Article.deleteOne({title: req.params.articleTitle}, function(err){
+    if(!err){
+      res.send("Sucess");
+    } else{
+      res.send("err");
+    }
+  });
+})
+
 
 app.listen(3000, function() {
   console.log("Server started on port 3000");
